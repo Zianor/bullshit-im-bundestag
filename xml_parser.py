881c6b149e78e151ea7decea672bd8525c8ca7f5
Nina Löseke <loeseke@itc.rwth-aachen.de>
2019-07-03 13:47:28 +0200

    Create list with one dict per comment specifying speaker and content

--- xml_parser.py
+++ xml_parser.py
@@ -7,7 +7,8 @@ with open('data/19105-data.xml', 'rb') as xml_file:
 
 root = tree.getroot()
 
-# nach Tagesordnungspunkten in Sitzungsverlauf suchen
+# extract data about speeces from XML protocol
+
 comment_list = []
 for topic in root[1]:
     if not 'top-id' in topic.attrib:
@@ -20,12 +21,37 @@ for topic in root[1]:
         if rede.tag != 'rede':
             continue
         #print(rede.tag, rede.attrib)
+        #print(rede)
         
-        for kommentar in rede:
-            if kommentar.tag == 'kommentar':
+        current_speaker = ''
+        
+        for subelement in rede:
+            
+            # extract metainformation about speaker
+            
+            if subelement.tag == 'p' and subelement.attrib['klasse'] == 'redner':
+                    
+                meta_dict = {}
+                    
+                for speaker_tag in subelement.iter():
+                    if speaker_tag.tag == 'vorname':
+                        meta_dict['vorname'] = speaker_tag.text
+                    if speaker_tag.tag == 'nachname':
+                        meta_dict['nachname'] = speaker_tag.text
+                    if speaker_tag.tag == 'fraktion':
+                        meta_dict['fraktion'] = speaker_tag.text
+                        current_speaker = meta_dict['fraktion']
+                    
+            # extract content of comment
+            
+            if subelement.tag == 'kommentar':
                 #print(kommentar.tag, kommentar.attrib)
-                k_text = kommentar.text.strip('()').replace(u'\xa0', u' ')
-                comment_list.append(k_text)
+                
+                if current_speaker is None:
+                    raise ValueError('Comment was found but no current speaker is set!')
+                
+                kommentar = subelement.text.strip('()').replace(u'\xa0', u' ')
+                comment_list.append({'redner': current_speaker, 'kommentar': kommentar})
                     
 print(comment_list)
 
